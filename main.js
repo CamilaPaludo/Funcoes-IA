7a0689423a6063bcba881fbc0cea2f73b4122492
Camila Paludo <107196927+CamilaPaludo@users.noreply.github.com>
2024-08-23 10:43:36 -0300

    23/08

--- main.js
+++ main.js
@@ -6,68 +6,87 @@ const textoResultado= document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: " Imagine que você tirou 30 dias de férias e pode escolher seu destino podendo levar quem quiser. Porém está com dúvida para onde ir, nós podemos te ajudar. Qual o clima de sua preferência? ",
         alternativas: [
             {
-                texto: " Texto/resposta",
-                afirmacao: " Afirmação ",
+                texto: " Lugares Quentes ",
+                afirmacao: " Lembre-se de levar protetor solar. ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Lugares Frios ",
+                afirmacao: " Lembre-se de levar agasalhos. ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " ótimo! Continuando com sua preferência, o lugar que deseja ir, seria...?",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Lugares agitados e movimentados ",
+                afirmacao: " Sempre fique atento, mantenha seus pertences próximos ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Lugares Calmos e aconchegantes ",
+                afirmacao: " Aproveite suas férias para tirar um descanso ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Maravilha!Me conte... Que vai te acompanhar? ",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Família e/ou pets ",
+                afirmacao: " Hotéis ou resort's são a melhor opção para você  ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Amigos ",
+                afirmacao: " AirBnb e empresas de turismo podem te auxliar ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Bom! De acordo com seu financeiro, quanto pretende gastar em sua viagem? ",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Até três salários mínimos ",
+                afirmacao: " Boa opção para você são viagens nacionais ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Acima de três salários mínimos ",
+                afirmacao: " Você irá adorar conhecer novos países ",
             }
         ]
     },
     {
-        enunciado: " Pergunta?",
+        enunciado: " Para Finalizar... Qual o meio de locomoção que você pretende utilizar para chegar ao seu destino? ",
         alternativas: [
             {
-                texto: " Texto/resposta ",
-                afirmacao: "Afirmação",
+                texto: " Carro ou Motocicletas ",
+                afirmacao: " Seu destino pode ser próximo e você ",
             },
             {
-                texto: " Texto/resposta ",
-                afirmacao: " Afirmação ",
+                texto: " Avião ou Õnibus ",
+                afirmacao: " Sair da sua zona de conforto e conhecer novos horizontes ",
             }
         ]
     },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta () {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement ("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        caixaAlternativas.appendChild (botaoAlternativas);
+    }
+}
+
+mostraPergunta();
